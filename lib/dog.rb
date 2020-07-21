2de97d41eeb005d5f5ce81c58ba246e0c4a168c2
Kinnari Shah <kinnari1022@yahoo.com>
2020-07-21 17:40:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,20 @@
 class Dog 
   
   def name= (dogs_name)
-    @name = "Fido"
+    @name = dogs_name
   end
   
   def name
     @name
   end
   
+  def breed= (dogs_breed)
+  @breed = dogs_breed
+  end
+  
+  def breed
+    @breed
+  end
 end
 
 # fido = Dog.new
